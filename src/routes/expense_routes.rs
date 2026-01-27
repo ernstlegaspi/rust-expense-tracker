e6420d517119a5abfb2c5b2cac02739849bc8359
Akie <legaspiernstdanielle@gmail.com>
2026-01-27 13:32:13 +0800

    feature: delete expense per user

--- src/routes/expense_routes.rs
+++ src/routes/expense_routes.rs
@@ -1,7 +1,8 @@
-use actix_web::web::{ServiceConfig, get, post, put, scope};
+use actix_web::web::{ServiceConfig, delete, get, post, put, scope};
 
 use crate::handlers::expense::{
-    add_expense, edit_expense_per_user, get_single_expense_per_user, get_user_expenses,
+    add_expense, delete_expense_per_user, edit_expense_per_user, get_single_expense_per_user,
+    get_user_expenses,
 };
 
 pub fn route(cfg: &mut ServiceConfig) {
@@ -10,6 +11,7 @@ pub fn route(cfg: &mut ServiceConfig) {
             .route("/", post().to(add_expense))
             .route("/user", get().to(get_user_expenses))
             .route("/user/{expense_id}", get().to(get_single_expense_per_user))
-            .route("/user/{expense_id}", put().to(edit_expense_per_user)),
+            .route("/user/{expense_id}", put().to(edit_expense_per_user))
+            .route("/user/{expense_id}", delete().to(delete_expense_per_user)),
     );
 }
